c0d67d1bbf0b517bdaa1d0154b6430d7afef1604
Flareonn <appavel2011@yandex.ru>
2022-04-01 01:34:13 +0500

    added part of /alisa commands

--- src/main/java/org/shy/alisa/CommandKit.java
+++ src/main/java/org/shy/alisa/CommandKit.java
@@ -8,9 +8,88 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.shy.alisa.listeners.VoteEvent;
 
-import java.util.EventListener;
-import java.util.Objects;
+import java.util.UUID;
 
+import static java.lang.String.format;
+
+class AlisaCommandBot implements CommandExecutor {
+    public static final Main ALISA = Main.getInstance();
+    public static Player iniciator;
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(commandSender instanceof Player) {
+            iniciator = (Player) commandSender;
+        }
+        if(strings.length == 0) {
+            commandSender.sendMessage(
+                    format("%s--------- %s[ADMIN]%s: %sАЛИСА%s -----------\n", ChatColor.YELLOW, ChatColor.DARK_RED, ChatColor.YELLOW, ChatColor.AQUA, ChatColor.YELLOW) +
+                        format("%s/alisa:%s Все комманды администратора\n", ChatColor.GOLD, ChatColor.WHITE) +
+                        format("%s/alisa read:%s Посмотреть значение в конфиге\n", ChatColor.GOLD, ChatColor.WHITE) +
+                        format("%s/alisa set:%s Изменить значение в конфиге\n", ChatColor.GOLD, ChatColor.WHITE) +
+                        format("%s/alisa reloadconfig:%s Перезагрузка конфига\n", ChatColor.GOLD, ChatColor.WHITE) +
+                        format("%s/alisa getname:%s Получить Ник игрока по UUID\n", ChatColor.GOLD, ChatColor.WHITE) +
+                        format("%s/alisa getuuid:%s Получить UUID игрока по Нику\n", ChatColor.GOLD, ChatColor.WHITE)
+                    );
+        } else {
+            Player player;
+            switch (strings[0]) {
+                case "read":
+                    ALISA.say(format("Значение в конфиге поля '%s': %s", ChatColor.LIGHT_PURPLE + strings[1] + ChatColor.YELLOW, ChatColor.GREEN + "\u00A7l" + ALISA.getConfig().getString(strings[1])), iniciator);
+                    break;
+                case "set":
+                    String configValue = strings[2];
+                    ALISA.getConfig().set(strings[1], isDigit(configValue) ? Integer.parseInt(configValue) : configValue);
+                    ALISA.saveConfig();
+                    ALISA.say(format("Вы установили поле '%s' со значением: %s", ChatColor.LIGHT_PURPLE + strings[1] + ChatColor.YELLOW, ChatColor.DARK_PURPLE + "\u00A7l" + configValue), iniciator);
+                    break;
+                case "reloadconfig":
+                    ALISA.reloadConfig();
+                    break;
+                case "getuuid":
+                case "getname":
+                    // WIP
+                    final UUID uuid = UUID.fromString(strings[1]);
+                    if(uuid != null) {
+                        player = Bukkit.getPlayer(uuid);
+                    } else {
+                        player = Bukkit.getPlayer(strings[1]);
+                    }
+
+                    if(player != null) {
+                        ALISA.say(format("Ник игрока: %s", player.getName()), iniciator);
+                        ALISA.say(format("UUID: %s", player.getUniqueId()), iniciator);
+                    } else {
+                        ALISA.say("Игрок с таким именем не найден", iniciator);
+                    }
+
+                    break;
+                case "tospawn":
+                    // WIP
+                    player = Bukkit.getPlayer(strings[1]);
+                    if(player != null) {
+                        Bukkit.getServer().dispatchCommand(player, "spawn");
+                        ALISA.say("Игрок будет отправлен на спавн", iniciator);
+                    } else {
+                        ALISA.say("Игрок с таким именем не найден", iniciator);
+                    }
+                    break;
+                default:
+                    ALISA.say("Такой команды не существует. Список моих возможностей: -> /alisa", iniciator);
+                    break;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isDigit(String s) throws NumberFormatException {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
 
 class AlisaCommandColors implements CommandExecutor {
     public static final Main ALISA = Main.getInstance();
