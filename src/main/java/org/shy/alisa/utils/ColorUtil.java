2c2aa5a9ebcb68b7dc2e894c5394eb2672375273
Flareonn <appavel2011@yandex.ru>
2022-04-04 02:23:31 +0500

    Created Color utils

--- src/main/java/org/shy/alisa/utils/ColorUtil.java
+++ src/main/java/org/shy/alisa/utils/ColorUtil.java
@@ -3,25 +3,59 @@ package org.shy.alisa.utils;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 
-public class AlisaColor {
+public class ColorUtil {
     private static String prefixColor;
     private static String nameColor;
     private static String textColor;
     private static String bracketColor;
+    private static String ALISA_TAG;
 
-    public AlisaColor(FileConfiguration fileConfiguration) {
-        prefixColor = fileConfiguration.getString("chat-colors.prefix-color");
-        nameColor = fileConfiguration.getString("chat-colors.name-color");
-        textColor = fileConfiguration.getString("chat-colors.text-color");
-        bracketColor = fileConfiguration.getString("chat-colors.bracket-color");
+
+    public ColorUtil(FileConfiguration fileConfiguration) {
+        prefixColor = translateColorCode(fileConfiguration.getString("chat-colors.prefix-color"));
+        nameColor = translateColorCode(fileConfiguration.getString("chat-colors.name-color"));
+        textColor = translateColorCode(fileConfiguration.getString("chat-colors.text-color"));
+        bracketColor = translateColorCode(fileConfiguration.getString("chat-colors.bracket-color"));
+        ALISA_TAG = String.format("%s[%sПомощница%s]%s Алиса:%s ", bracketColor, prefixColor, bracketColor, nameColor, textColor);
     }
 
-    public String getTag() {
-        String bracketsColorCode = translateColorCode(bracketColor);
-        return String.format("%s[%sПомощница%s]%s Алиса:%s ", bracketsColorCode, translateColorCode(prefixColor), bracketsColorCode, translateColorCode(nameColor), translateColorCode(textColor));
+    public static String getAlisaTag() {
+        return ALISA_TAG;
     }
 
-    private String translateColorCode(String color) {
+    private static String translateColorCode(String color) {
         return ChatColor.translateAlternateColorCodes('&', "&" + color);
+    }
+
+    public static String getBracketColor() {
+        return bracketColor;
+    }
+
+    public static String getNameColor() {
+        return nameColor;
+    }
+
+    public static String getPrefixColor() {
+        return prefixColor;
+    }
+
+    public static String getTextColor() {
+        return textColor;
+    }
+
+    public static String success(String word) {
+        return wrap(word, ChatColor.GREEN, ChatColor.BOLD);
+    }
+
+    public static String fail(String word) {
+        return wrap(word, ChatColor.RED, ChatColor.BOLD);
+    }
+
+    public static String wrap(String word, ChatColor color) {
+        return color + word + ChatColor.RESET + getTextColor();
+    }
+
+    public static String wrap(String word, ChatColor color, ChatColor modifiers) {
+        return color + "" + modifiers + word + ChatColor.RESET + getTextColor();
     }
 }
