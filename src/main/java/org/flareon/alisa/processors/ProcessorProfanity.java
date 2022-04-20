c5830e12d1bc5f55f15dcb987af2a9da3ac6a787
Flareonn <appavel2011@yandex.ru>
2022-04-21 01:11:51 +0500

    Процессор нецензурной брани

--- src/main/java/org/flareon/alisa/processors/ProcessorProfanity.java
+++ src/main/java/org/flareon/alisa/processors/ProcessorProfanity.java
@@ -1,2 +1,42 @@
-package org.flareon.alisa.processors;public class ProcessorProfanity {
+package org.flareon.alisa.processors;
+
+import java.util.*;
+import org.bukkit.entity.*;
+import org.flareon.alisa.FLAlisa;
+import org.flareon.alisa.utils.FileUtil;
+
+import java.util.regex.*;
+
+public class ProcessorProfanity implements IProcessor
+{
+    protected FLAlisa ALISA;
+    ArrayList<Pattern> badWords;
+
+    public ProcessorProfanity(final FLAlisa ALISA) {
+        this.ALISA = ALISA;
+        final ArrayList<String> temp = FileUtil.readProjectFileLines("profanity.txt");
+        this.badWords = new ArrayList<>(temp.size());
+        for (final String word : temp) {
+            final Pattern pat = Pattern.compile(word.replace("|", "\\b"));
+            this.badWords.add(pat);
+        }
+    }
+
+    @Override
+    public boolean processMessage(final Player player, final String playerMesage) {
+        final String message = playerMesage.toLowerCase();
+        for (final Pattern p : this.badWords) {
+            final Matcher matcher = p.matcher(message);
+            if (matcher.find()) {
+                this.ALISA.punish(player, this.getTempmuteDurationProfanity(), "3.2 (нецензурная брань)", FLAlisa.AnswerReason.PROFANITY);
+                this.ALISA.debug("mute/profanity: " + player.getName() + ": '" + message + "' (" + playerMesage + ")");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int getTempmuteDurationProfanity() {
+        return this.ALISA.config.getInt("tempmute.profanity");
+    }
 }
