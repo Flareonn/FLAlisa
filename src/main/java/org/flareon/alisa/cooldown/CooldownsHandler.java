0ddf243d886307d968c0b49c01daf3d691da67b3
Flareonn <appavel2011@yandex.ru>
2022-04-21 01:10:24 +0500

    Кулдауны на приветствие и варн

--- src/main/java/org/flareon/alisa/cooldown/CooldownsHandler.java
+++ src/main/java/org/flareon/alisa/cooldown/CooldownsHandler.java
@@ -5,13 +5,17 @@ import org.flareon.alisa.Config;
 public class CooldownsHandler {
     public Cooldown votesunGlobalCooldown;
     public Cooldown votedayGlobalCooldown;
+    public Cooldown helloByeCooldown;
     public CooldownPlayerBased votesunPersonalCooldowns;
     public CooldownPlayerBased votedayPersonalCooldowns;
+    public CooldownPlayerBased warnCooldowns;
 
     public CooldownsHandler(Config config) {
+        this.helloByeCooldown = new Cooldown(config.getInt("cooldown.hello"));
         this.votesunGlobalCooldown = new Cooldown(config.getInt("cooldown.votesun-global"));
         this.votedayGlobalCooldown = new Cooldown(config.getInt("cooldown.voteday-global"));
         this.votesunPersonalCooldowns = new CooldownPlayerBased(config.getInt("cooldown.votesun-personal"));
         this.votedayPersonalCooldowns = new CooldownPlayerBased(config.getInt("cooldown.voteday-personal"));
+        this.warnCooldowns = new CooldownPlayerBased(config.getInt("cooldown.warn"));
     }
 }
