f82dd5394dface6414a2a7ae53bdee6426a14938
Flareonn <appavel2011@yandex.ru>
2022-04-21 01:12:04 +0500

    Процессор флуда символами

--- src/main/java/org/flareon/alisa/processors/ProcessorSymbolFlood.java
+++ src/main/java/org/flareon/alisa/processors/ProcessorSymbolFlood.java
@@ -1,2 +1,46 @@
-package org.flareon.alisa.processors;public class ProcessorSymbolFlood {
+package org.flareon.alisa.processors;
+
+import org.bukkit.entity.*;
+import org.flareon.alisa.FLAlisa;
+
+public class ProcessorSymbolFlood implements IProcessor
+{
+    private final FLAlisa ALISA;
+    private final int minimumMessageLengthForSymbolCheck;
+
+    public ProcessorSymbolFlood(final FLAlisa ALISA) {
+        this.minimumMessageLengthForSymbolCheck = 12;
+        this.ALISA = ALISA;
+    }
+
+    @Override
+    public boolean processMessage(final Player player, final String message) {
+        if (message.length() >= this.minimumMessageLengthForSymbolCheck && this.getSymbolRatioOfMessage(message) > this.getAllowedSymbolRatio()) {
+            this.ALISA.punish(player, this.getTempmuteDurationSymbolFlood(), "3.1 (флуд символами)", FLAlisa.AnswerReason.FLOOD);
+            return true;
+        }
+        return false;
+    }
+
+    private int getTempmuteDurationSymbolFlood() {
+        return this.ALISA.config.getInt("tempmute.symbol-flood");
+    }
+
+    public float getSymbolRatioOfMessage(final String message) {
+        if (message == null || message.trim().isEmpty()) {
+            return 0.0f;
+        }
+        int count = 0;
+        final String specialChars = "/*!@#$%^&*()\"{}_[]|\\?/<>,.№;:+=-'";
+        for (int i = 0; i < message.length(); ++i) {
+            if (specialChars.contains(message.substring(i, i + 1))) {
+                ++count;
+            }
+        }
+        return count / (float)message.length();
+    }
+
+    private float getAllowedSymbolRatio() {
+        return this.ALISA.config.getFloat("symbol-ratio");
+    }
 }
