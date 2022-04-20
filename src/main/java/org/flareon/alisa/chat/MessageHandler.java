c0bf31d16521937b510ce8de14bffa4b1bbe9a7f
Flareonn <appavel2011@yandex.ru>
2022-04-21 01:09:49 +0500

    Обработчик сообщений

--- src/main/java/org/flareon/alisa/chat/MessageHandler.java
+++ src/main/java/org/flareon/alisa/chat/MessageHandler.java
@@ -1,2 +1,43 @@
-package org.flareon.alisa.chat;public class MessageHandler {
+package org.flareon.alisa.chat;
+
+import org.bukkit.entity.Player;
+import org.flareon.alisa.FLAlisa;
+import org.flareon.alisa.processors.*;
+
+import java.util.ArrayList;
+
+public class MessageHandler {
+    private final ArrayList<IProcessor> globalMessageProcessors;
+    private final ArrayList<IProcessor> localMessageProcessors;
+    public MessageHandler(final FLAlisa ALISA) {
+        this.globalMessageProcessors = new ArrayList<IProcessor>() {{
+            add(new ProcessorCaps(ALISA));
+            add(new ProcessorFlood(ALISA));
+            add(new ProcessorSymbolFlood(ALISA));
+            add(new ProcessorProfanity(ALISA));
+            add(new ProcessorHelloBye(ALISA));
+        }};
+        this.localMessageProcessors = new ArrayList<>();
+    }
+
+    public void handleMessage(final Player player, final String playerMessage) {
+        final boolean isGlobal = this.isGlobalMessage(playerMessage);
+        if (isGlobal) {
+            String message = this.deColor(playerMessage);
+            message = message.substring(1);
+            for (final IProcessor processor : this.globalMessageProcessors) {
+                if (processor.processMessage(player, message)) {
+                    return;
+                }
+            }
+        }
+    }
+    private boolean isGlobalMessage(final String message) {
+        return message.indexOf("!") == 0;
+    }
+
+    private String deColor(final String msg) {
+        return msg.replaceAll("§.", "");
+    }
+
 }
